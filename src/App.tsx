8747450946a62e4338f79b17f05b988212d11d4a
HaderCabrera <hader.cabrera96@gmail.com>
2025-01-09 17:03:38 -0500

    reeamplazando

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
+
 import '@aws-amplify/ui-react/styles.css';
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
